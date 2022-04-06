09a502c9a5a74459d2ab9872647aa34b16a2e7be
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-04-06 19:04:08 +0530

    Variable declaration

---
+++ Fundamentals/js/app.js
@@ -0,0 +1,20 @@
+// declare variables
+
+let js = "awesome";
+
+if (js === "awesome") {
+  alert("Yess!!!, Javascript is awesome");
+}
+
+// console log
+
+console.log(12 + 34 + 23 - 133 * 4);
+console.log("TheSysCoder");
+
+// variables
+
+let name = "TheSysCoder"; //never use name as variable name because it's reserved keyword in javascript
+console.log(name);
+
+let firstName = "";
+let first_name = "";
