708ff009738c0b0cb1d71130b047bd4064413b6a
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-04-07 18:27:20 +0530

    operators

--- Fundamentals/js/app.js
+++ Fundamentals/js/app.js
@@ -31,3 +31,36 @@ console.log(typeof age);
 let fruit = "Apple";
 console.log(fruit);
 console.log(typeof fruit);
+
+// let and const
+
+let ages = 24;
+console.log(ages);
+
+ages = 42;
+console.log(ages);
+
+// const variable declaration
+
+const birthYear = 1994;
+console.log(birthYear);
+
+// const variable called as immutable variables
+
+// Operators in javascript
+
+const now = 2022;
+
+const ageJake = now - 1980;
+const ageSarah = now - 1987;
+console.log(ageJake, ageSarah);
+
+console.log(ageJake * 2, ageSarah / 10, 2 ** 5);
+
+// assignment operator
+
+let x = 10;
+
+x += 23;
+
+console.log(x);
