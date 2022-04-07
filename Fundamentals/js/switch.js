48865d0ff2777ed3c1cf0758139560ba0698f953
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-04-07 21:36:03 +0530

    switch

---
+++ Fundamentals/js/switch.js
@@ -0,0 +1,15 @@
+// switch case in javascript
+
+const day = "monday";
+
+switch (day) {
+  case "sunday":
+    console.log("Heyyyy!!! its a weekend");
+    break;
+  case "monday":
+    console.log("oh noooo!!!, its monday again");
+    break;
+  default:
+    console.log("Invalid day, enter monday or sunday only");
+    break;
+}
